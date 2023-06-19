6b83ba5b4a4a2c074bf3b50b0d98b18865c584d1
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-06-19 10:06:19 -0700

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,62 @@
+var youtube = "https://www.youtube.com/channel/UCJWp7G3LM2-b2tFMT-eyrAA";
+var linkedin = "https://www.linkedin.com/in/shabari-k-s-56421822a/";
+var instagram = "https://www.instagram.com/_.sourcecode._/";
+var github = "https://github.com/Shabari_K_S/";
+var email = 'mailto:forrest@fkcodes.com';
+
+whois = [
+    "<br>",
+    "Source Code is a software development company that specializes in building web and mobile applications.",
+    "<br>"
+];
+
+whoami = [
+  "<br>",
+  "I don't know who you are? but we know why your here.",
+  "<br>"
+];
+
+social = [
+    "<br>",
+    "YouTube: <a href=\"" + youtube + "\" target=\"_blank\">youtube</a>",
+    "LinkedIn: <a href=\"" + linkedin + "\" target=\"_blank\">linkedin</a>",
+    "Instagram: <a href=\"" + instagram + "\" target=\"_blank\">instagram</a>",
+    "GitHub: <a href=\"" + github + "\" target=\"_blank\">github</a>",
+    "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+    "<br>",
+    "Available commands:",
+    "<br>",
+    "whois           Learn more about Source Code",
+    "whoami          Learn more about you",
+    "social          See social media links",
+    "projects        See projects",
+    "video           Watch a video",
+    "email           Send an email",
+    "clear           Clear the terminal",
+    "history         See command history",
+    "banner          See the banner",
+    "help            See this list",
+    "<br>"
+];
+
+banner = [
+  ' ███████╗ ██████╗ ██╗   ██╗██████╗  ██████╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗ ',
+  ' ██╔════╝██╔═══██╗██║   ██║██╔══██╗██╔════╝██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝ ',
+  ' ███████╗██║   ██║██║   ██║██████╔╝██║     █████╗      ██║     ██║   ██║██║  ██║█████╗   ',
+  ' ╚════██║██║   ██║██║   ██║██╔══██╗██║     ██╔══╝      ██║     ██║   ██║██║  ██║██╔══╝   ',
+  ' ███████║╚██████╔╝╚██████╔╝██║  ██║╚██████╗███████╗    ╚██████╗╚██████╔╝██████╔╝███████╗ ',
+  ' ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝ ',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "============================================================================================================================================================================================================================================================================="
+];
