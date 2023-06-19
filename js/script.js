bb59dd46e8d25d567f92bcb98998b793b178e058
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-06-19 06:11:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
 const aboutcontent = document.querySelector('#about-content');
 const contactcontent = document.querySelector('#contact-content');
+const projects = document.querySelector('#project');
+const projectscontent = document.querySelector('#projects');
 
 about.addEventListener('click', () => {
     const aboutbox = new WinBox({
-        title: 'about-me$:',
+        title: 'shabari@s0urc3c0d3:~/about-me$:',
         width: '400px',
         height: '400px',
         top: 10,
@@ -31,7 +33,7 @@ about.addEventListener('click', () => {
 
 contact.addEventListener('click', () => {
     const contactbox = new WinBox({
-        title: 'contact-me$:',
+        title: 'shabari@s0urc3c0d3:~/contact-me$:',
         width: '400px',
         height: '400px',
         top: 10,
@@ -39,6 +41,25 @@ contact.addEventListener('click', () => {
         bottom: 10,
         left: 10,
         mount: contactcontent,
+        onfocus: function () {
+            this.setBackground('#034f03')
+        },
+        onblur: function () {
+            this.setBackground('#000')
+        }
+    })
+})
+
+projects.addEventListener('click', () => {
+    const projectsbox = new WinBox({
+        title: 'shabari@s0urc3c0d3:~/projects$:',
+        width: '400px',
+        height: '400px',
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10,
+        mount: projectscontent,
         onfocus: function () {
             this.setBackground('#034f03')
         },
