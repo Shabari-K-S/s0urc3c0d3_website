57d21c961f4efd0db77d2759ac3028a1f47f6022
Shabari-K-S <shabaricse2003@gmail.com>
2024-10-23 19:42:48 +0530

    ui update

--- js/main.js
+++ js/main.js
@@ -84,7 +84,6 @@ function commander(cmd) {
         terminal.innerHTML = '<a id="before"></a>';
         before = document.getElementById("before");
       }, 1);
-      break;
     case "banner":
       loopLines(banner, "", 80);
       break;
